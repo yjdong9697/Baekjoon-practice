bfc3080447d86251fc74abdd6b796dbcd42476d1
yjdong9697 <yjdong9697@yonsei.ac.kr>
2020-07-28 20:06:03 +0900

    2020.07.28 ps (Greedy algorithm)

---
+++ baekjoon_11047.py
@@ -0,0 +1,31 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+
+N_data = []
+
+
+# Input N_data (make list)
+
+for i in range(N):
+    data = int(input())
+    N_data.append(data)
+
+# Calculate Inversely
+
+count = 0
+for i in range(1,11):
+
+    # If K == 0 makes early (break) 
+
+    if K == 0:
+        break
+
+    if K >= N_data[-i]:
+        count += K // N_data[-i]
+        K = K % N_data[-i]
+        
+
+# Print the result
+    
+print(count)
