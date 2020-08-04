dd7bf901203756b26f279d2e3e5bfade28f032f3
yjdong9697 <yjdong9697@yonsei.ac.kr>
2020-08-04 20:04:15 +0900

    2020.08.04 ps (D.P)

---
+++ baekjoon_13699.py
@@ -0,0 +1,45 @@
+import sys
+
+def recur(n):
+    """
+    This function is store the recurrence relation value
+    If value is already exist in list = reuse it
+    else: calculate it and store it
+    """
+
+    #  If not exist
+
+    if recur_store[n - 1] == 0:
+        recur(n - 1)
+        result = 0
+        for i in range(n):
+            result += recur_store[i] * recur_store[n - 1 - i]
+        
+        recur_store[n] = result
+
+        return
+
+    # If exist
+
+    else:
+        result = 0
+        for i in range(n):
+            result += recur_store[i] * recur_store[n - 1 - i]
+
+        recur_store[n] = result
+        
+        return
+
+
+# Initial setting
+
+n = int(input())
+
+recur_store = [0] * (n + 1)
+recur_store[0] = 1
+
+recur(n)
+
+# Print the result
+
+print(recur_store[n])
